19521f5deabc60fcaf6833ed6ab993ff72a9d5ee
Naafiul Razzaq <naafiulrazzaq@gmail.com>
2023-12-14 18:08:59 +0700

    add migration for create view pos_users

--- database/migrations/2023_12_06_071532_add_shop_open_shop_close_to_settings_table.php
+++ database/migrations/2023_12_06_071532_add_shop_open_shop_close_to_settings_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -11,9 +12,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        
         Schema::table('settings', function (Blueprint $table) {
-            $table->time('shop_open')->nullable()->after('emp_can_delete');
-            $table->time('shop_close')->nullable()->after('shop_open');
+            $table->time('shop_open')->nullable()->default(Carbon::createFromTime('07','00','00'))->after('emp_can_delete');
+            $table->time('shop_close')->nullable()->default(Carbon::createFromTime('22','00','00'))->after('shop_open');
         });
     }
 
